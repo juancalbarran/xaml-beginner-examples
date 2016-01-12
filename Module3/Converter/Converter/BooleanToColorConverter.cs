14ed5bb5b9c49dd4bbd899e5e266be560e2d430b
juan.albarran <tazjc2505@gmail.com>
2016-01-12 08:18:29 -0800

    Module 3

---
+++ Module3/Converter/Converter/BooleanToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace Converter
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Colors.Green : Colors.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Module3/CustomBehavior/ShowDialogBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+
+namespace CustomBehavior
+{
+    public class ShowDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+
+        public DependencyObject AssociatedObject
+        {
+            get;
+            private set;
+        }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            this.AssociatedObject = associatedObject;
+            (this.AssociatedObject as Button).Click += Button_Click;
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Button).Click -= Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.AssociatedObject is Button)
+                (this.AssociatedObject as Button).Click -= Button_Click;
+        }
+    }
+}
