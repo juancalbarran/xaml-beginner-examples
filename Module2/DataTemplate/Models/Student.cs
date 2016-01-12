835e850ccdfda04d1a69d8c55999639ce4d9e08c
juan.albarran <tazjc2505@gmail.com>
2016-01-12 08:16:30 -0800

    Module 2

---
+++ Module2/DataTemplate/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI;
+
+namespace DataTemplate.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public Color FavoriteColor { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ Module2/DataTemplate/ViewModels/ClassroomViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DataTemplate.Models;
+using System.Collections.Generic;
+using Windows.UI;
+
+namespace DataTemplate.ViewModels
+{
+    public class ClassroomViewModel
+    {
+        public string ClassroomName { get; private set; }
+
+        public List<Student> Students { get; private set; }
+
+        public ClassroomViewModel()
+        {
+            ClassroomName = "Ms. Henderson's 5th Grade Class";
+            Students = new List<Student>
+        {
+            new Student { Name = "Sidney Andrews", FavoriteColor = Colors.Gold, Age = 9 },
+            new Student { Name = "Jaiden Ashby", FavoriteColor = Colors.Red, Age = 10 }
+        };
+        }
+    }
+}
