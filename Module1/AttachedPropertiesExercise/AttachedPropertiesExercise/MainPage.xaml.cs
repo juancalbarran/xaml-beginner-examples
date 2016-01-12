ca19ed55b10ecfc7686b078d2c6ccdeb8c31c057
juan.albarran <tazjc2505@gmail.com>
2016-01-12 08:14:34 -0800

    Module 1

---
+++ Module1/AttachedPropertiesExercise/AttachedPropertiesExercise/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AttachedPropertiesExercise1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadData();
+        }
+
+        async void LoadData()
+        {
+            this.SetValue(LoadingProperties.IsLoadingProperty, true);
+            await Task.Delay(5000);
+            this.SetValue(LoadingProperties.IsLoadingProperty, false);
+        }
+    }
+}
